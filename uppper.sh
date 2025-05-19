2f9130606ad2f5d7831b64b3adec892db02c259f
rohit <rohit.vishwakarma@msbcgroup.com>
2025-05-20 02:22:35 +0530

    remove script

--- uppper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-while read -p "Enter favorite movie names (or 'exit' to stop): " movie; do
-  if [[ "${movie}" == "exit" ]]; then
-    break
-  fi
-  declare -u movie_upper="${movie}"
-  echo "${movie_upper}"
-done
